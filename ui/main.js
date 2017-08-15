8acef9b04802294bc400353ecfff11f92ef5dd6b
siva1github <30726430+siva1github@users.noreply.github.com>
2017-08-14 19:47:34 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ element.innerHTML='New value1';
 var img=document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-  marginLeft = marginLeft + 5;
+  marginLeft = marginLeft + 1;
   img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick=function() {
