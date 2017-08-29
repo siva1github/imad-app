9dfb3d49703bfd9712cea68f7077d1c276eb4c79
siva1github <30726430+siva1github@users.noreply.github.com>
2017-08-28 20:03:23 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,8 +130,20 @@ app.get('/articles/:articleName', function (req, res) {
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
     
-    
-  res.send(createTemplate(articles[articleName]));
+ 
+   pool.query("SELECT * FROM article WHERE title= " + req.params.articleName, function (err, result) {
+       if (err) {
+          res.status(500).send(err.toString());
+        }else
+        {
+            if(result.rows.length === 0){
+                res.status(404).send('article not found');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+   });
 });
 
 app.get('/ui/style.css', function (req, res) {
