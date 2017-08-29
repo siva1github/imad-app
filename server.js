ffbd8b99a8d4c6b9439f3851c752493308eedc70
siva1github <30726430+siva1github@users.noreply.github.com>
2017-08-28 17:23:03 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var config = {
+const config = {
     user: 'gsrkpr',
     database: 'gsrkpr',
     host: 'db.imad.hasura-app.io',
