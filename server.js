92e837e2669c6a2886ea794cc346b8eeb62df7bd
siva1github <30726430+siva1github@users.noreply.github.com>
2017-08-28 17:39:45 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,10 +125,12 @@ app.get('/submit-name', function(req, res){ //URL: /submit-name?name=xxxxx
      res.send(JSON.stringify(names)); //ToDO
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
+    
+    
   res.send(createTemplate(articles[articleName]));
 });
 
