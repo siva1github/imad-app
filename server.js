8900e6a15b9fb0c7d65626e191eb8fdc2dd9b1c6
siva1github <30726430+siva1github@users.noreply.github.com>
2017-09-01 17:16:47 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool('config');
+var pool = new Pool(config);
 app.get('/test-db', function(req,res){
    //make a select request
    pool.query('SELECT * FROM test', function(err, result){
