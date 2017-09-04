43a4f9162b7dd5617d1a48eb98324fc4e5fdb2fc
siva1github <30726430+siva1github@users.noreply.github.com>
2017-09-04 10:32:25 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'gsrkpr',
@@ -13,6 +14,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var articles = {
     'article-one':
@@ -113,9 +115,11 @@ app.get('/hash/:input',function (req, res) {
 
 app.post('/create-user',function (req, res) {
     //username, password
+    //"username";, siva, "password":, "password"
+    
     var username = req.body.username;
     var password = req.body.password;
-    var salt = crypto.getRandomBytes(128).toString('hex');
+    var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password,salt);
     pool.query('INSERT INTO "user" (username,password) VALUES ($1, $2)', [username,dbString], function(err, result){
        if (err) {
