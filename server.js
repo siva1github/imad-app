6dc5f85036b02c700ef4046cef96b96e799331c7
siva1github <30726430+siva1github@users.noreply.github.com>
2017-09-04 16:33:18 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ app.post('/login',function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
 
-    pool.query('SELECT * from "user" username = $1', [username], function(err, result){
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result){
        if (err) {
           res.status(500).send(err.toString());
         }else
@@ -152,7 +152,9 @@ app.post('/login',function (req, res) {
                 var salt = dbString.split[2];
                 var hashedPassword = hash(password,salt); //creating a hash based on the password submitted and the original salt
                 if (hashedPassword === dbString) {
-                    res.send('user successfully created ' + username);
+                    res.send('credentials are correct');
+                }else{
+                    res.send(403).send('user name is invalid');   
                 }
             }
         }        
