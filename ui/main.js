2ed5bed7142b4bce02baebc1102333151aeb276a
siva1github <30726430+siva1github@users.noreply.github.com>
2017-09-05 15:17:24 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,6 @@
-//Counter code
-var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function(){
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            //Take some action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        //Not done yet
-    };
-    //Make a request
-    request.open('GET', 'http://gsrkpr.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
 
-//submit name
+
+//submit username/password tp login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
      //Create a request object
@@ -33,23 +11,23 @@ submit.onclick = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             //Take some action
             if(request.status === 200){
-                   //capture a list of namesand render it as list
-                  var names = request.responseText;
-                  names = JSON.parse(names);
-                  var list = '';
-                  for (var i=0; i<names.length; i++){
-                      list += '<li>' + names[i] + '</li>';
-                  }
-                  var ul = document.getElementById('namelist');
-                  ul.innerHTML = list;
+                   console.log('user logged in');
+                   alert('logged in successfully');
                
+            } else if(request.status === 403){
+                alert('username/password is incorrect');
+            }else if(request.status === 500){
+                alert('something went wrong in the server');
             }
         }
         //Not done yet
     };
     //Make a request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://gsrkpr.imad.hasura-app.io/submit-name?name=' + name,true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://gsrkpr.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
