360e43e7ce9a0caad2d67cdc26d21f6ea0ca83d2
siva1github <30726430+siva1github@users.noreply.github.com>
2017-09-06 17:09:10 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,5 +25,5 @@ submit.onclick = function(){
     console.log(password);
     request.open('POST', 'http://gsrkpr.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({"username": username, "password": password}));
+    request.send(JSON.stringify({username: username, password: password}));
 };
